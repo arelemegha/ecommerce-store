f3734bfc0d798a5f89e153cf977d7ded618a0c0a
arelemegha <arelemegha@gmail.com>
2021-02-01 17:27:39 +0530

    My First Website - Ecommerce Store

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="container">
+        <p style="font-size:16px;" align="center">Copyright © Megha Arele.Anand Engineering College| R.No.1800110035</p>
+    </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,78 @@
+<?php 
+require 'includes/common.php';
+if (isset($_SESSION['email'])) 
+    { 
+    header('location: product.php'); 
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Lifestyle Store</title>
+        <meta charset="UTF-8">
+        <meta name="description" content="LifeStyle Store India's faishon e-store."/>
+        <meta name="keywords" content="camera, watches, shirts"/>
+        <meta name="author" content="Bipul Kumar">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css" type="text/css">
+        <script type="text/javascript" src="bootstrap/js/jquery-3.3.1.min.js"></script>
+        <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script>
+        <link href="css/index.css" rel="stylesheet" type="text/css"/>
+    </head>
+    <body>
+        <?php
+        include 'includes/header.php';
+        ?>
+        <div id="banner_image">
+            <div class="inner-banner-image">
+                <center>
+                    <div id="banner_content">
+                        <h1>We sell lifestyle.</h1>
+                        <p>Flat 10% OFF on premium brands</p>
+                        <a href="product.php" class="btn btn-danger btn-lg active">Shop Now</a>
+                    </div>
+                </center>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4">
+                    <a href="product.php#Cameras" class="thumbnail">
+                        <img src="img/camera.jpg" alt="camera"/>
+                        <center>
+                        <div class="caption">
+                            <h2>Cameras</h2> 
+                            <p>Choose among the best available in the world.</p>
+                        </div>
+                        </center>
+                    </a>
+                </div>
+                <div class="col-sm-4">
+                    <a href="product.php#Watches" class="thumbnail">
+                        <img src="img/watch.jpg" alt="watch"/>
+                        <center>
+                        <div class="caption">
+                            <h2>Watches</h2> 
+                            <p>Original watches from the best brands.</p>
+                        </div>  
+                        </center>
+                    </a>
+                </div>
+                <div class="col-sm-4">
+                    <a href="product.php#Shirts" class="thumbnail">
+                        <img src="img/shirt.jpg" alt="shirt"/>
+                        <center>
+                        <div class="caption">
+                            <h2>Shirts</h2> 
+                            <p>Our exquisite collection of shirts.</p>
+                        </div>        
+                        </center>
+                    </a>                   
+                </div>
+            </div>
+        </div>
+        <?php
+        include 'includes/footer.php';
+        ?>
+    </body>
+</html>
